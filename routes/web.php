32d3685c88790424a2ebdc5ec4c82af179e50461
Hai-TranC1019i1 <tranhaicoder@gmail.com>
2020-03-19 15:00:49 +0700

    complete login social

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('', function () {
+   return view('welcome');
 });
+
+Route::get('/redirect/{social}', 'SocialAuthController@redirect');
+Route::get('/callback/{social}', 'SocialAuthController@callback');
