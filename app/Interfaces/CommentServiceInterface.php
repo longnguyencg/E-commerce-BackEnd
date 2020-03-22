41975442896151f54ba9217b05b30fd3ef788c54
Hai-TranC1019i1 <tranhaicoder@gmail.com>
2020-03-22 21:07:08 +0700

    complete comment

---
+++ app/Interfaces/CommentServiceInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Interfaces;
+
+
+interface CommentServiceInterface extends ServiceInterface
+{
+
+}
---
+++ database/migrations/2020_03_22_130304_create_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('reply_user_id')->nullable();
+            $table->text('content');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('reply_user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
